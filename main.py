1d4f965611ce3039e3de3be8453de3344e42784a
Леонид Литвинов <leonidlitvinoff@yandex.ru>
2020-04-04 16:50:49 +0700

    Task three

--- main.py
+++ main.py
@@ -1,27 +1,14 @@
 import vk_api
-from datetime import datetime
+import os
 
 LOGIN = ''
 PASSWORD = ''
-def main():
-    login, password = LOGIN, PASSWORD
-    vk_session = vk_api.VkApi(login, password)
-    try:
-        vk_session.auth(token_only=True)
-    except vk_api.AuthError as error_msg:
-        print(error_msg)
-        return
-    vk = vk_session.get_api()
-    # Используем метод wall.get
-    response = response = vk.friends.get(fields="first_name, last_name, bdate")
-    if response['items']:
-        for i in sorted(response['items'], key=lambda x: x['last_name']):
-            print(i['last_name'], i['first_name'], end=' ')
-            a = ''
-            if 'bdate' in i:
-                a = i['bdate']
-            print(a)
-
-
-if __name__ == '__mai n__':
-    main()
+files = list(map(lambda x: f'static/img/{x}', os.listdir('static/img')))
+login, password = LOGIN, PASSWORD
+vk_session = vk_api.VkApi(login, password)
+vk_session.auth(token_only=True)
+upload  = vk_api.VkUpload(vk_session)
+photo = upload.photo_wall(files)
+vk_photo_id = [f"photo{i['owner_id']}_{i['id']}" for i in photo]
+vk = vk_session.get_api()
+vk.wall.post(message="Test", attachments=vk_photo_id)
