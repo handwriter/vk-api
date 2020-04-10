186fa7acdb247d419246923ad030bd1cc4f6a0eb
Леонид Литвинов <leonidlitvinoff@yandex.ru>
2020-04-10 22:15:08 +0700

    Task four]

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
-import vk_api
-import os
+from vk_api import VkApi
+from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
+import random
+import requests
+from auth import TOKEN
 
-LOGIN = ''
-PASSWORD = ''
-files = list(map(lambda x: f'static/img/{x}', os.listdir('static/img')))
-login, password = LOGIN, PASSWORD
-vk_session = vk_api.VkApi(login, password)
-vk_session.auth(token_only=True)
-upload  = vk_api.VkUpload(vk_session)
-photo = upload.photo_wall(files)
-vk_photo_id = [f"photo{i['owner_id']}_{i['id']}" for i in photo]
-vk = vk_session.get_api()
-vk.wall.post(message="Test", attachments=vk_photo_id)
+
+vk_session = VkApi(token=TOKEN)
+longpoll = VkBotLongPoll(vk_session, '193813979')
+
+
+def main():
+    for event in longpoll.listen():
+
+        if event.type == VkBotEventType.MESSAGE_NEW:
+            vk = vk_session.get_api()
+            request = requests.get(f'https://api.vk.com/method/users.get?user_ids={event.obj.message["from_id"]}&fields=city&access_token={TOKEN}&v=5.103').json()
+            vk.messages.send(user_id=event.obj.message['from_id'],
+                             message=f"Привет, {request['response'][0]['first_name']}",
+                             random_id=random.randint(0, 2 ** 64))
+            if 'city' in request['response'][0]:
+                vk.messages.send(user_id=event.obj.message['from_id'],
+                                 message=f"Как поживает город {request['response'][0]['city']['title']}",
+                                 random_id=random.randint(0, 2 ** 64))
+
+
+if __name__ == '__main__':
+    main()
